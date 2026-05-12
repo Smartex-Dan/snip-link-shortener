f5211e2bf229d08960bf4808646253d06b380aba
Smartex_Dan <adeoyedan100@gmail.com>
2026-05-12 04:33:26 +0100

    Wannna Sleeep!

--- app.py
+++ app.py
@@ -94,5 +94,5 @@ def redirect_link(code):
 # ── Entry ──────────────────────────────────────────────────────────────────────
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(debug=False, host="0.0.0.0", port=port)
